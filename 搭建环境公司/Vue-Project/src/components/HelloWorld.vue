312733952ab4c0a3400f51674e490648edd0f5a5
zhishuangyu <zhishuang.yu@mxsoft.com>
2018-01-09 18:07:46 +0800

    公司

---
+++ 搭建环境公司/Vue-Project/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    demo () {
+      console.log('学习JavaScript数据结构与算法')
+    }
+  },
+  created () {
+    this.demo()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
